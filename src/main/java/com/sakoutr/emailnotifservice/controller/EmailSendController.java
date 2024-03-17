3f2f4987785b38caaa0e7fbee475c2222765819b
sakout-reda <sakout-reda@hotmail.fr>
2024-03-17 14:32:58 +0000

    SecurityConfig

--- src/main/java/com/sakoutr/emailnotifservice/controller/EmailSendController.java
+++ src/main/java/com/sakoutr/emailnotifservice/controller/EmailSendController.java
@@ -1,6 +1,7 @@
 package com.sakoutr.emailnotifservice.controller;
 
 import com.sakoutr.emailnotifservice.service.EmailService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class EmailSendController {
     @PostMapping("/send")
     public String sendMail(String to, String[] cc, String subject, String body) {
         return emailService.sendMail( to, cc, subject, body);
+    }
+
+    @GetMapping
+    public String isSecured(){
+        return "Hello World";
     }
 
 }
---
+++ src/main/java/com/sakoutr/emailnotifservice/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sakoutr.emailnotifservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf ->csrf.disable())
+                .oauth2ResourceServer(ors->ors.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
